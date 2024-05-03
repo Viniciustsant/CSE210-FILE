36868704cc255f19a92945f4fe8c1fc13f982fc8
Vinicius Teixeira Santiago <viniciustsantiagosud@gmail.com>
2024-05-03 17:38:45 -0300

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        int guess = -1;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,100);
+
+
+
+        while (guess != number)
+            {
+                Console.WriteLine("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+
+                if (number == guess)
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+
+                else if (guess > number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+
+                else if (guess < number )
+                    {
+                        Console.WriteLine("Higher");
+                    }
+            }
     }
 }
